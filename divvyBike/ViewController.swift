4d43f2de63d39ce0fcdd26385c33588815824263
Gavin Uhran <GUhran@Gavins-Macbook-Air-2.local>
2017-06-26 10:24:46 -0500

    gorger

--- divvyBike/ViewController.swift
+++ divvyBike/ViewController.swift
@@ -8,13 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
 {
     @IBOutlet weak var myTableView: UITableView!
+    var stations = [[String: String]]()
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return stations.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = myTableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        let station = stations[indexPath.row]
+        cell.textLabel?.text = station["location"]
+        cell.detailTextLabel?.text = station["dock"]
+        return cell
     }
 }
 

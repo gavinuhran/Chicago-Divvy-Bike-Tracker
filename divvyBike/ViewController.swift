7c7111f3f1e8b48210be8c5b536d2af147c4365a
Gavin Uhran <GUhran@Gavins-Macbook-Air-2.local>
2017-06-26 10:20:02 -0500

    vdsbgjksad

--- divvyBike/ViewController.swift
+++ divvyBike/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController
 {
+    @IBOutlet weak var myTableView: UITableView!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
